5e9c485607e74f3f0574f09bf10194a1596c5f18
Tom Morelly <tom-morelly@gmx.de>
2021-08-18 10:51:55 +0200

    add installation error msg

--- run_tfproviderlint.sh
+++ run_tfproviderlint.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+[[ "$(command -v tfproviderlint)" ]] || { echo "\"tfproviderlint\" not installed. Install via \"go get github.com/bflad/tfproviderlint/cmd/tfproviderlint\"" >&2; exit 1; }
+
 exec tfproviderlint ./...
--- run_tfproviderlintx.sh
+++ run_tfproviderlintx.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+[[ "$(command -v tfproviderlintx)" ]] || { echo "\"tfproviderlintx\" not installed. Install via \"go get github.com/bflad/tfproviderlint/cmd/tfproviderlintx\"" >&2; exit 1; }
+
 exec tfproviderlintx ./...
